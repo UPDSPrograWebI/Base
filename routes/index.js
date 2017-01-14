f493fee63e01fbfa9c3b1611f614d7b946963726
Carlos Alberto Gomez Ormachea <betto.gomez@icloud.com>
2017-01-13 20:39:03 -0400

    Removed Jade Library

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // http://localhost:3000/
-  res.render('index', { title: 'Express' }); //JADE
-});
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // http://localhost:3000/users/
-  res.json({
-    name: "Alberto"
-  });
-});
-
 module.exports = router;
